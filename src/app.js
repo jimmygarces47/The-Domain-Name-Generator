36f8ac2c90d6c10bca6be92fa826e2b3a2bc4e76
jimmygarces47 <jimmygarces4@gmail.com>
2022-02-10 01:07:36 +0000

    realice el generador de dominios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.getElementById("domain").innerHTML = resultado;
+
+  let resultado = "";
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var domin = [".com", ".org", ".co"];
+
+  for (let i = 0; i <= pronoun.length - 1; i++) {
+    for (let j = 0; j <= adj.length - 1; j++) {
+      for (let k = 0; k <= noun.length - 1; k++) {
+        for (let l = 0; l <= domin.length - 1; l++) {
+          resultado += pronoun[i] + adj[j] + noun[k] + domin[l] + "</br>";
+          console.log(resultado);
+        }
+      }
+    }
+  }
+
+  document.getElementById("domain").innerHTML = resultado;
 };
